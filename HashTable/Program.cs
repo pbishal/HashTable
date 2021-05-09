ef6ac3d28cbcb822d5ce8f7d2bba08446a18e0ca
pbishal <pbishal60@gmail.com>
2021-05-09 22:21:44 +0530

    Found Frequency of Words In A Large Paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -14,6 +14,11 @@ namespace HashTable
             MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             hash.Add("0", "To be or not to be");
             hash.frequencyOfWords("0");
+
+            //Passing paragraph and finding frequency
+            hash.Add("1", "Paranoids are not Paranoids because they are Paranoids but because they keep putting themselves deliberatly into paranoids avoidable situations");
+            hash.frequencyOfWords("1");
+
             Console.ReadLine();
         }
     }
