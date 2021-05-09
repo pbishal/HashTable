828c81e65ceadd1c8f4016c874cc9806310a7251
pbishal <pbishal60@gmail.com>
2021-05-09 22:14:46 +0530

    Ability to find frequency

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -10,6 +10,11 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to HashTable");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To be or not to be");
+            hash.frequencyOfWords("0");
+            Console.ReadLine();
         }
     }
 }
